39eb7ac4e01e8e3821b6c511e49f49eee82fcb14
Brunom8218 <bmars8218@gmail.com>
2024-01-09 08:46:14 -0800

    240109.0 add open graph for SEO

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Page from 'components/Page'
+import ScrollSection from 'components/home/ScrollSection'
+
+
+const Home: FC = () => {
+  return (
+    <Page>
+      <ScrollSection />
+    </Page>
+  )
+}
+
+export default Home
